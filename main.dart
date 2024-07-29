f6cd8802cec7addc47adaee9aac9309a0afb9e1a
Radu Eduard <66945274+ediradu@users.noreply.github.com>
2024-07-29 17:37:46 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,39 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:like_button/like_button.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Hello World'),
-        backgroundColor: Colors.red[700],
-        centerTitle: true,
-      ),
-      body: const Home(),
-    ),
+    home: MyHomePage(),
   ));
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _page = 0;
+  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
-      padding: const EdgeInsets.all(20),
-      margin: const EdgeInsets.fromLTRB(10, 40, 0, 0),
-      child: const Text(
-        'Hello World',
-        style: TextStyle(
-          fontSize: 16,
-          letterSpacing: 4,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Un stat pe spate'),
+        backgroundColor: Colors.red[700],
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.blueAccent,
+        child: Home(pageIndex: _page),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: 0,
+        height: 60.0,
+        items: <Widget>[
+          Icon(Icons.add, size: 30),
+          Icon(Icons.list, size: 30),
+          Icon(Icons.compare_arrows, size: 30),
+        ],
+        color: Colors.white,
+        buttonBackgroundColor: Colors.white,
+        backgroundColor: Colors.blueAccent,
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 600),
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+        letIndexChange: (index) => true,
       ),
     );
   }
 }
 
+class Home extends StatelessWidget {
+  final int pageIndex;
 
-// build(BuildContext context) {
-//     return const Text('Neata!!!');
-//   }
+  const Home({Key? key, required this.pageIndex}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Page Index: $pageIndex',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+          if (pageIndex == 0)
+            CounterWidget()
+          else if (pageIndex == 1)
+            TextFieldWidget()
+          else
+            LikeButtonWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Număr de apăsări: $_counter',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: _incrementCounter,
+          child: Text('Apasă-mă'),
+        ),
+      ],
+    );
+  }
+}
+
+class TextFieldWidget extends StatefulWidget {
+  @override
+  _TextFieldWidgetState createState() => _TextFieldWidgetState();
+}
+
+class _TextFieldWidgetState extends State<TextFieldWidget> {
+  String _inputText = "";
+
+  void _updateText(String text) {
+    setState(() {
+      _inputText = text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: TextField(
+            onChanged: _updateText,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Introdu text',
+              filled: true,
+              fillColor: Colors.white,
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Text introdus: $_inputText',
+          style: TextStyle(fontSize: 16, color: Colors.white),
+        ),
+      ],
+    );
+  }
+}
+
+class LikeButtonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: LikeButton(
+        size: 80,
+      ),
+    );
+  }
+}
